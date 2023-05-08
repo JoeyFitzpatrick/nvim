4679080df500609f1aaa9dc28bb4ff39b0b9b325
fitzpj3 <joey.fitzpatrick@vanderbilt.edu>
2023-05-07 22:14:56 -0500

    Improved remaps

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -59,7 +59,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -68,7 +68,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set({'n', 'x'}, '<leader>fm', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
+  vim.keymap.set({'n', 'x'}, '<leader>F', function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end, opts)
 end)
 
 lsp.nvim_workspace()
--- lua/non-vscode.lua
+++ lua/non-vscode.lua
@@ -6,14 +6,11 @@ vim.g.mapleader = " "
 
 -- telescope settings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-vim.keymap.set('n', '<leader>q', builtin.quickfix, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>H', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 vim.keymap.set('n', '<C-p>', ":lua require'telescope'.extensions.project.project{}<CR>",
 { noremap = true, silent = true })
 
@@ -48,14 +45,3 @@ vim.keymap.set('n', '<Leader>lg', ':LazyGit<CR>', { silent = true })
 -- nvim-tree settings
 vim.keymap.set('n', '<Leader>n', ';NvimTreeToggle<CR>', { remap = true })
 vim.keymap.set('n', '<Leader>N', ';NvimTreeFocus<CR>', { remap = true })
-
--- fzf settings
-vim.keymap.set('n', '<C-f>', ':Files<CR>', { silent = true })
--- vim.keymap.set('n', '<Leader>rg', ':Rg<CR>', { silent = true })
--- don't consider filename as match
--- command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)
-
--- command! -bang -nargs=* Rg
---   \ call fzf#vim#grep(
---   \   'rg --hidden --column --line-number --no-heading --color=always --smart-case -- '.shellescape(<q-args>), 1,
---   \   fzf#vim#with_preview(), <bang>0)
