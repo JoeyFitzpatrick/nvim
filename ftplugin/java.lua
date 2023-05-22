dfc01af795863c4128be59d312928c761e4f5a9d
Joey Fitzpatrick <=>
2023-05-21 22:53:26 -0500

    Committing before some big changes

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,6 +2,7 @@
      cmd = {
          '/Users/fitzpj3/.local/opt/jdtls-launcher/jdtls/bin/jdtls',
          -- '/Library/Java/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home/bin/java',
+         -- 'java',
          -- '-Declipse.application=org.eclipse.jdt.ls.core.id1',
          -- '-Dosgi.bundles.defaultStartLevel=4',
          -- '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -11,6 +12,7 @@
          -- '--add-modules=ALL-SYSTEM',
          -- '--add-opens', 'java.base/java.util=ALL-UNNAMED',
          -- '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+         '-javaagent:$HOME/.local/lib/lombok/lombok.jar',
          -- '-jar', '/Users/fitzpj3/eclipse/jee-2023-03/Eclipse.app/Contents/Eclipse/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
      },
      root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
