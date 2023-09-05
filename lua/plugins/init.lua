04b5df14deced4f49b601a6e7736abe0dbf065dd
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2023-09-04 22:26:12 -0500

    Saving before new machine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -162,5 +162,31 @@ return {
 		opts = {},
 	},
 	{ "https://github.com/windwp/nvim-ts-autotag" },
-}
+	{
+		"https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			-- This module contains a number of default definitions
+			local rainbow_delimiters = require("rainbow-delimiters")
 
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					vim = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+	},
+}
