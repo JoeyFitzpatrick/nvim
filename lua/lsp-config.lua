ed908feae8856b5408dc2d7831cbf756f82f75eb
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2023-10-17 22:00:19 -0500

    Removing java support
    java

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -6,97 +6,97 @@ local opts = { silent = true, noremap = true }
 vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
 lsp.preset({
-    name = "minimal",
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
+	name = "minimal",
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false,
 })
 
 local servers = {
-    "pyright",
-    "svelte",
-    "rust_analyzer",
-    "lua_ls",
-    "eslint",
+	"pyright",
+	"svelte",
+	"rust_analyzer",
+	"lua_ls",
+	"eslint",
 }
 
 -- More servers can be found here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 lsp.ensure_installed(servers)
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({
-        buffer = bufnr,
-        preserve_mappings = false,
-    })
+	lsp.default_keymaps({
+		buffer = bufnr,
+		preserve_mappings = false,
+	})
 end)
 
 local lspconfig = require("lspconfig")
 -- example to setup lua_ls and enable call snippets
 lspconfig.lua_ls.setup({
-    settings = {
-        Lua = {
-            completion = {
-                callSnippet = "Replace",
-            },
-            diagnostics = {
-                globals = {"vim"},
-      },
-        },
-    },
+	settings = {
+		Lua = {
+			completion = {
+				callSnippet = "Replace",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 -- More servers can be found here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 lsp.setup_servers(servers)
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function()
-        vim.lsp.buf.definition()
-    end, opts)
-    vim.keymap.set("n", "gi", function()
-        vim.lsp.buf.implementation()
-    end, opts)
-    vim.keymap.set("n", "<leader>ho", function()
-        vim.lsp.buf.hover()
-    end, opts)
-    vim.keymap.set("n", "<leader>vws", function()
-        vim.lsp.buf.workspace_symbol()
-    end, opts)
-    vim.keymap.set("n", "<leader>vd", function()
-        vim.diagnostic.open_float()
-    end, opts)
-    vim.keymap.set("n", "[d", function()
-        vim.diagnostic.goto_next()
-    end, opts)
-    vim.keymap.set("n", "]d", function()
-        vim.diagnostic.goto_prev()
-    end, opts)
-    vim.keymap.set("n", "<leader>ca", function()
-        vim.lsp.buf.code_action()
-    end, opts)
-    -- vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>rn", function()
-        vim.lsp.buf.rename()
-    end, opts)
-    vim.keymap.set("i", "<C-h>", function()
-        vim.lsp.buf.signature_help()
-    end, opts)
-    vim.keymap.set({ "n", "x", "v" }, "<leader>F", function()
-        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-    end, opts)
-    vim.keymap.set("n", "<leader>ic", function()
-        vim.lsp.buf.incoming_calls()
-    end, opts)
-    vim.keymap.set("n", "<leader>oc", function()
-        vim.lsp.buf.outgoing_calls()
-    end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "gi", function()
+		vim.lsp.buf.implementation()
+	end, opts)
+	vim.keymap.set("n", "<leader>ho", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>ca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	-- vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+	vim.keymap.set({ "n", "x", "v" }, "<leader>F", function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, opts)
+	vim.keymap.set("n", "<leader>ic", function()
+		vim.lsp.buf.incoming_calls()
+	end, opts)
+	vim.keymap.set("n", "<leader>oc", function()
+		vim.lsp.buf.outgoing_calls()
+	end, opts)
 end)
 
 vim.diagnostic.config({
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
 })
 
 lsp.nvim_workspace()
@@ -110,60 +110,59 @@ local cmp_action = require("lsp-zero").cmp_action()
 require("luasnip.loaders.from_snipmate").lazy_load()
 
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<Enter>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<Tab>"] = {
-        i = cmp.config.disable,
-        c = cmp.config.disable,
-    },
+	["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+	["<Enter>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+	["<Tab>"] = {
+		i = cmp.config.disable,
+		c = cmp.config.disable,
+	},
 })
 
 local cmp_sources = {
-    { name = "nvim_lsp" },
-    { name = "cmp_luasnip" },
-    -- { name = "luasnip" },
+	{ name = "nvim_lsp" },
+	{ name = "cmp_luasnip" },
+	-- { name = "luasnip" },
 }
 
 cmp.setup({
-    mapping = cmp_mappings,
-    sources = cmp_sources,
+	mapping = cmp_mappings,
+	sources = cmp_sources,
 })
-
 
 local sonar_path = "~/.local/share/nvim/mason/share/sonarlint-analyzers"
 require("sonarlint").setup({
-    server = {
-        cmd = {
-            "sonarlint-language-server",
-            -- Ensure that sonarlint-language-server uses stdio channel
-            "-stdio",
-            "-analyzers",
-            -- paths to the analyzers you need, using those for python and java in this example
-            vim.fn.expand(sonar_path .. "/sonarpython.jar"),
-            vim.fn.expand(sonar_path .. "/sonarjava.jar"),
-            vim.fn.expand(sonar_path .. "/sonarjs.jar"),
-        },
-    },
-    filetypes = {
-        "python",
-        "typescript",
-        "typescriptreact",
-        "javascript",
-        -- Requires nvim-jdtls, otherwise an error message will be printed
-        "java",
-    },
+	server = {
+		cmd = {
+			"sonarlint-language-server",
+			-- Ensure that sonarlint-language-server uses stdio channel
+			"-stdio",
+			"-analyzers",
+			-- paths to the analyzers you need, using those for python and java in this example
+			vim.fn.expand(sonar_path .. "/sonarpython.jar"),
+			-- vim.fn.expand(sonar_path .. "/sonarjava.jar"),
+			vim.fn.expand(sonar_path .. "/sonarjs.jar"),
+		},
+	},
+	filetypes = {
+		"python",
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		-- Requires nvim-jdtls, otherwise an error message will be printed
+		-- "java",
+	},
 })
 
 require("helpers.linters")
 
 local diagnostics_active = true
-vim.keymap.set('n', '<leader>ty', function()
-  diagnostics_active = not diagnostics_active
-  if diagnostics_active then
-    vim.diagnostic.show()
-  else
-    vim.diagnostic.hide()
-  end
+vim.keymap.set("n", "<leader>ty", function()
+	diagnostics_active = not diagnostics_active
+	if diagnostics_active then
+		vim.diagnostic.show()
+	else
+		vim.diagnostic.hide()
+	end
 end)
---
+++ unused/java.lua
@@ -0,0 +1,139 @@
+-- put this all in ftplugins/java.lua for java lsp support.
+-- don't forget to add jdtls plugin, and possibly comrade plugin
+local home = "/Users/fitzpj3"
+local jdtls = require("jdtls")
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local root_markers = { "gradlew", "mvnw", ".git" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+
+-- local workspace_folder = home .. '/.local/share/eclipse/' .. project_name
+local workspace_folder = home .. "/Tools/nvim-eclipse-workspaces/" .. project_name
+-- vim.keymap.set('n', '<leader>i', function() print(workspace_folder) end)
+
+local config = {
+	flags = {
+		debounce_text_changes = 80,
+	},
+	root_dir = root_dir, -- Set the root directory to our found root_marker
+	init_options = {
+		extendedClientCapabilities = extendedClientCapabilities,
+	},
+	settings = {
+		java = {
+			format = {
+				settings = {
+					-- Use Google Java style guidelines for formatting
+					-- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+					-- and place it in the ~/.local/share/eclipse directory
+					url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+					profile = "GoogleStyle",
+				},
+			},
+			signatureHelp = { enabled = true },
+			contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
+			-- Specify any completion options
+			eclipse = {
+				downloadSources = true,
+			},
+			maven = {
+				downloadSources = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			completion = {
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+					"org.mockito.Mockito.*",
+				},
+				filteredTypes = {
+					"com.sun.*",
+					"io.micrometer.shaded.*",
+					"java.awt.*",
+					"jdk.*",
+					"sun.*",
+				},
+			},
+			-- Specify any options for organizing imports
+			sources = {
+				organizeImports = {
+					starThreshold = 9999,
+					staticStarThreshold = 9999,
+				},
+			},
+			-- How code generation should act
+			codeGeneration = {
+				toString = {
+					template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+				},
+				hashCodeEquals = {
+					useJava7Objects = true,
+				},
+				useBlocks = true,
+			},
+			configuration = {
+				runtimes = {
+					{
+						name = "JavaSE-1.8",
+						path = "/Users/fitzpj3/Tools/java-stuff/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home",
+					},
+					{
+						name = "JavaSE-11",
+						path = "/Users/fitzpj3/Tools/java-stuff/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home",
+					},
+					{
+						name = "JavaSE-17",
+						path = "/Users/fitzpj3/Tools/java-stuff/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home",
+					},
+				},
+			},
+		},
+	},
+	cmd = {
+		"java",
+		-- "/Users/fitzpj3/Tools/java-stuff/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home/bin/java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-javaagent:/Users/fitzpj3/.config/nvim/dependencies/lombok.jar",
+		-- "-Xbootclasspath/a:/Users/fitzpj3/.config/nvim/dependencies/lombok.jar",
+		"-Xmx4g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+
+		-- The jar file is located where jdtls was installed. This will need to be updated
+		-- to the location where you installed jdtls
+		"-jar",
+		vim.fn.glob("/Users/fitzpj3/jdt-language-server-1.9.0-202203031534/plugins/org.eclipse.equinox.launcher_*.jar"),
+
+		-- The configuration for jdtls is also placed where jdtls was installed. This will
+		-- need to be updated depending on your environment
+		"-configuration",
+		"/Users/fitzpj3/jdt-language-server-1.9.0-202203031534/config_mac",
+
+		-- Use the workspace_folder defined above to store data for this project
+		"-data",
+		workspace_folder,
+	},
+}
+
+-- Finally, start jdtls. This will run the language server using the configuration we specified,
+-- setup the keymappings, and attach the LSP client to the current buffer
+jdtls.start_or_attach(config)(
+
+	-- put the below in plugins to use!
+	{ "mfussenegger/nvim-jdtls", lazy = true }
+)
