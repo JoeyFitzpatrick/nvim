655949611523383ab9e456f432b6c11c779a2c10
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-01-27 22:48:39 -0600

    remove ts parser for c

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,6 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python" },
+	ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
