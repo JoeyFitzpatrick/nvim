883daec9d584e4672e102bb26917afc9c9b5d499
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-02-28 15:17:45 -0600

    chore: fix butterfish remaps

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -18,14 +18,14 @@ return {
 	{
 		"bakks/butterfish.nvim",
 		keys = {
-			{ "<leader>ap", "<cmd>BFFilePrompt ", desc = "Butterfish File Prompt" },
-			{ "<leader>ar", "<cmd>BFRewrite ", mode = { "n", "v" }, desc = "Butterfish Rewrite" },
-			{ "<leader>ac", "<cmd>BFComment<CR>", mode = { "n", "v" }, desc = "Butterfish Comment" },
-			{ "<leader>ae", "<cmd>BFExplain<CR>", mode = { "n", "v" }, desc = "Butterfish Explain" },
-			{ "<leader>af", "<cmd>BFFix<CR>", desc = "Butterfish Fix" },
-			{ "<leader>ai", "<cmd>BFImplement<CR>", desc = "Butterfish Implement" },
-			{ "<leader>ad", "<cmd>BFEdit ", desc = "Butterfish Edit" },
-			{ "<leader>aq", "<cmd>BFQuestion ", desc = "Butterfish Question" },
+			{ "<leader>ap", ":BFFilePrompt ", desc = "Butterfish File Prompt" },
+			{ "<leader>ar", ":BFRewrite ", mode = { "n", "v" }, desc = "Butterfish Rewrite" },
+			{ "<leader>ac", ":BFComment<CR>", mode = { "n", "v" }, desc = "Butterfish Comment" },
+			{ "<leader>ae", ":BFExplain<CR>", mode = { "n", "v" }, desc = "Butterfish Explain" },
+			{ "<leader>af", ":BFFix<CR>", desc = "Butterfish Fix" },
+			{ "<leader>ai", ":BFImplement<CR>", desc = "Butterfish Implement" },
+			{ "<leader>ad", ":BFEdit ", desc = "Butterfish Edit" },
+			{ "<leader>aq", ":BFQuestion ", desc = "Butterfish Question" },
 		},
 		config = function()
 			require("butterfish")
