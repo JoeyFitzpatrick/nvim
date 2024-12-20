204e5b5eff71aba3cb3615b824c4db30eb4bf376
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2024-12-19 22:16:07 -0600

    chore: update blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -123,46 +123,55 @@ return {
 		lazy = false, -- lazy loading handled internally
 
 		-- use a release tag to download pre-built binaries
-		version = "v0.6.0",
+		version = "v0.*",
 		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
+		build = "cargo build --release",
 		opts = {
-			keymap = {
-				preset = "default",
-				["<C-y>"] = { "accept" },
-				["<C-p>"] = { "select_prev" },
-				["<Up>"] = { "select_prev" },
-				["<C-n>"] = { "select_next", "show" },
-				["<Down>"] = { "select_next" },
-				["<C-b>"] = { "scroll_documentation_up" },
-				["<C-f>"] = { "scroll_documentation_down" },
-			},
-			highlight = {
+			appearance = {
 				-- sets the fallback highlight groups to nvim-cmp's highlight groups
 				-- useful for when your theme doesn't support blink.cmp
 				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = true,
+				use_nvim_cmp_as_default = false,
+				-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
 			},
-			sources = {
-				completion = {
-					enabled_providers = { "lsp", "path" },
+			completion = {
+				accept = {
+					-- experimental auto-brackets support
+					auto_brackets = {
+						enabled = true,
+					},
 				},
-			},
-			nerd_font_variant = "normal",
-			windows = {
-				autocomplete = {
-					selection = "auto_insert",
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+					},
 				},
 				documentation = {
 					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = vim.g.ai_cmp,
 				},
 			},
 
-			-- experimental auto-brackets support
-			-- accept = { auto_brackets = { enabled = true } }
-
 			-- experimental signature help support
-			-- trigger = { signature_help = { enabled = true } }
+			-- signature = { enabled = true },
+
+			sources = {
+				-- adding any nvim-cmp sources here will enable them
+				-- with blink.compat
+				compat = {},
+				default = { "lsp", "path", "snippets", "buffer" },
+				cmdline = {},
+			},
+
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
 		},
 	},
 }
