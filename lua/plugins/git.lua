fda87df97c22d139cf389995e0c24966af684c6c
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-02-20 09:55:30 -0600

    chore: add mergetool keymap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,6 +8,7 @@ return {
 			vim.keymap.set("n", "g<space>", ":G ", { desc = "Ever Prefix" })
 			vim.keymap.set("n", "<leader>je", "<cmd>G<CR>", { desc = "Open Ever Home UI" })
 			vim.keymap.set("n", "<leader>jd", "<cmd>G difftool<CR>", { desc = "Open Ever Difftool" })
+			vim.keymap.set("n", "<leader>jm", "<cmd>G mergetool<CR>", { desc = "Open mergetool" })
 			vim.keymap.set("n", "<leader>jt", "<cmd>G mergetool<CR>", { desc = "Open Ever for testing" })
 			vim.keymap.set("n", "<leader>rb", "<Plug>(Ever-resolve-base)", { desc = "Ever merge conflict base" })
 			vim.keymap.set("n", "<leader>ro", "<Plug>(Ever-resolve-ours)", { desc = "Ever merge conflict ours" })
