bccf83f2665753098ccb4e21ea04fedeb6ddd281
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-02-28 22:40:52 -0600

    chore: setup noice for just cmdline and lsp

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,32 +1,13 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	enabled = false,
 	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		routes = {
-			{
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-					},
-				},
-				view = "mini",
-			},
-		},
+		messages = { enabled = false },
+		popupmenu = { enabled = false },
+		notify = { enabled = false },
 		presets = {
 			bottom_search = true,
 			command_palette = true,
-			long_message_to_split = true,
 		},
 	},
 	dependencies = { "MunifTanjim/nui.nvim" },
