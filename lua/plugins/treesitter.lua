bc82cba4b9061df32d8d90cc20e0193f4a8275a3
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-03-03 22:10:37 -0600

    feat: add treewalker

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -85,4 +85,20 @@ return {
 			end, { silent = true, desc = "Toggle treesitter context" })
 		end,
 	},
+	{
+		"aaronik/treewalker.nvim",
+		config = function()
+			-- movement
+			vim.keymap.set({ "n", "v" }, "<A-k>", "<cmd>Treewalker Up<cr>", { silent = true })
+			vim.keymap.set({ "n", "v" }, "<A-j>", "<cmd>Treewalker Down<cr>", { silent = true })
+			vim.keymap.set({ "n", "v" }, "<A-h>", "<cmd>Treewalker Left<cr>", { silent = true })
+			vim.keymap.set({ "n", "v" }, "<A-l>", "<cmd>Treewalker Right<cr>", { silent = true })
+
+			-- swapping
+			vim.keymap.set("n", "<A-S-k>", "<cmd>Treewalker SwapUp<cr>", { silent = true })
+			vim.keymap.set("n", "<A-S-j>", "<cmd>Treewalker SwapDown<cr>", { silent = true })
+			vim.keymap.set("n", "<A-S-h>", "<cmd>Treewalker SwapLeft<cr>", { silent = true })
+			vim.keymap.set("n", "<A-S-l>", "<cmd>Treewalker SwapRight<cr>", { silent = true })
+		end,
+	},
 }
