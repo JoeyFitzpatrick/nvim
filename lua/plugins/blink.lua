50a89d81884c1bc89176415b345e2f809be0825c
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2025-11-25 22:29:36 -0600

    feat: added copilot plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,10 +5,6 @@ return {
 		event = "BufEnter",
 		opts = {
 			appearance = {
-				-- sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = false,
 				-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
