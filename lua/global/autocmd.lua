d098159a7095279ff05cd9b6e53653699d83a5b7
Joey Fitzpatrick <josephfitzpatrick333@yahoo.com>
2026-07-09 22:49:06 -0500

    feat: autocmd to auto-download treesitter parsers

--- lua/global/autocmd.lua
+++ lua/global/autocmd.lua
@@ -45,3 +45,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		end
 	end,
 })
+
+-- auto install treesitter parsers
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(ev)
+		local lang = vim.treesitter.language.get_lang(ev.match)
+		local available_langs = require("nvim-treesitter").get_available()
+		local is_available = vim.tbl_contains(available_langs, lang)
+		if is_available then
+			local installed_langs = require("nvim-treesitter").get_installed()
+			local installed = vim.tbl_contains(installed_langs, lang)
+			if not installed then
+				require("nvim-treesitter").install(lang):wait()
+			end
+			vim.treesitter.start()
+			require("nvim-treesitter").indentexpr()
+		end
+	end,
+})
