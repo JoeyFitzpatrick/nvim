647d69688f1744c03b74a6b8261afdc5d9875384
Joseph Fitzpatrick <josephfitzpatrick333@yahoo.com>
2026-09-18 20:43:38 -0500

    Autocmd to improve vimdiff colors (maybe?)

--- lua/global/autocmd.lua
+++ lua/global/autocmd.lua
@@ -59,3 +59,25 @@ vim.api.nvim_create_autocmd("FileType", {
 		end
 	end,
 })
+
+-- Improve vimdiff colors
+vim.cmd([[
+augroup diffcolors
+    autocmd!
+    autocmd Colorscheme * call s:SetDiffHighlights()
+augroup END
+
+function! s:SetDiffHighlights()
+    if &background == "dark"
+        highlight DiffAdd gui=bold guifg=none guibg=#2e4b2e
+        highlight DiffDelete gui=bold guifg=none guibg=#4c1e15
+        highlight DiffChange gui=bold guifg=none guibg=#45565c
+        highlight DiffText gui=bold guifg=none guibg=#996d74
+    else
+        highlight DiffAdd gui=bold guifg=none guibg=palegreen
+        highlight DiffDelete gui=bold guifg=none guibg=tomato
+        highlight DiffChange gui=bold guifg=none guibg=lightblue
+        highlight DiffText gui=bold guifg=none guibg=lightpink
+    endif
+endfunction
+]])
